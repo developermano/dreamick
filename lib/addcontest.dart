2204e662b4b38b4d0dc99d733b53cddd8c4a1a01
developermano <appdevelopermano@gmail.com>
2022-02-12 19:25:21 +0530

    web production

---
+++ lib/addcontest.dart
@@ -0,0 +1,85 @@
+import 'package:dreamprivatecontest/contest.dart';
+import 'package:dreamprivatecontest/main.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+
+class Addcontest extends StatefulWidget {
+  @override
+  _AddcontestState createState() => _AddcontestState();
+}
+
+class _AddcontestState extends State<Addcontest> {
+  var matchnamecon = new TextEditingController();
+  var entrycon = new TextEditingController();
+  var totalspotcon = new TextEditingController();
+  var linkcon = new TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Add contest"),
+      ),
+      body: Container(
+        margin: EdgeInsets.symmetric(vertical: 8, horizontal: 10),
+        child: Column(
+          children: [
+            TextFormField(
+              keyboardType: TextInputType.name,
+              controller: matchnamecon,
+              decoration: const InputDecoration(
+                hintText: 'IND VS AUS',
+                labelText: 'match name',
+              ),
+            ),
+            TextFormField(
+              keyboardType: TextInputType.number,
+              controller: entrycon,
+              decoration: const InputDecoration(
+                hintText: '5',
+                labelText: 'entryfee',
+              ),
+            ),
+            TextFormField(
+              keyboardType: TextInputType.number,
+              controller: totalspotcon,
+              decoration: const InputDecoration(
+                hintText: '5',
+                labelText: 'total spot',
+              ),
+            ),
+            TextFormField(
+              keyboardType: TextInputType.url,
+              controller: linkcon,
+              decoration: const InputDecoration(
+                hintText: 'https://dream11.com/....',
+                labelText: 'contest link',
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 10),
+              width: MediaQuery.of(context).size.width,
+              child: ElevatedButton(
+                  onPressed: () {
+                    if (matchnamecon.text != "" &&
+                        entrycon.text != "" &&
+                        totalspotcon.text != "" &&
+                        linkcon.text != "") {
+                      FirebaseDatabase.instance.ref("contest").push().set({
+                        "matchname": matchnamecon.text,
+                        "entryfee": entrycon.text,
+                        "totalspot": totalspotcon.text,
+                        "contesturl": linkcon.text,
+                        "createat":
+                            DateTime.now().toUtc().microsecondsSinceEpoch
+                      });
+                      Navigator.pop(context);
+                    }
+                  },
+                  child: Text("ADD CONTEST")),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/contest.dart
@@ -0,0 +1,10 @@
+class Contest {
+  final String matchname;
+  final String entryfee;
+  final String totalspot;
+  final String contesturl;
+  final int createat;
+
+  Contest(this.matchname, this.entryfee, this.totalspot, this.contesturl,
+      this.createat);
+}
